122e1f5819f79867d107fb9bbbef782d562a0c58
mohammadroshan621 <mohammadroshan621@gmail.com>
2024-01-10 15:03:32 +0530

    ep01

---
+++ App.js
@@ -0,0 +1,23 @@
+
+const parent = React.createElement(
+    "div",
+    {id: "parent"},
+    React.createElement(
+        "div",
+        {id:"child"},
+       [ React.createElement("h1",{},"im an h1 tag"),
+        React.createElement("h1",{},"im an h2 tag")
+    ]),
+
+    React.createElement(
+        "div",
+        {id:"child"},
+       [ React.createElement("h1",{},"im an h1 tag"),
+        React.createElement("h1",{},"im an h2 tag")
+    ]),
+      
+);
+console.log(parent)
+        const root = ReactDOM.createRoot(document.getElementById("root"));
+
+        root.render(parent);
