d45ec5ee962494a36395839a441445a05eb278ca
mohammadroshan621 <mohammadroshan621@gmail.com>
2024-01-11 11:32:47 +0530

    epi01

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ const parent = React.createElement(
        [ React.createElement("h1",{},"im an h1 tag"),
         React.createElement("h1",{},"im an h2 tag")
     ]),
-      
+      console.log(mohamad roshan)
 );
 console.log(parent)
         const root = ReactDOM.createRoot(document.getElementById("root"));
